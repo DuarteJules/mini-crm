d8fc890185be43ac23a8d8c795d618bb1b3a9413
Jules <jules.duarte@edu.esiee-it.fr>
2025-09-22 17:00:04 +0200

    Refactors CRM functionalities for better UX
    Improves the user experience by implementing interactive prompts using a buffered reader for cleaner input handling.
    
    The changes refactor the main loop and add functions for adding, displaying, deleting, and modifying contacts. This provides a more structured and maintainable codebase.
    
    A README file is also included to provide instructions on how to use the CRM.

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # mini-cli
+
+Membre du groupe : Jules Duarte
+
+Projet de mini-crm en CLI.
+
+Pour lancer le projet utiliser la ligne de commande go run main.go
+
+Afin de créer un prospect grace au flag utiliser la synthaxe suivante : 
+- go run main.go -add=true -email="exemple" -name="exemple"
--- main.go
+++ main.go
@@ -1,20 +1,26 @@
 package main
 
 import (
+	"bufio"
 	"flag"
 	"fmt"
+	"os"
+	"strconv"
+	"strings"
 )
 
-//TIP <p>To run your code, right-click the code and select <b>Run</b>.</p> <p>Alternatively, click
+// TIP <p>To run your code, right-click the code and select <b>Run</b>.</p> <p>Alternatively, click
 // the <icon src="AllIcons.Actions.Execute"/> icon in the gutter and select the <b>Run</b> menu item from here.</p>
+type contact struct {
+	email string
+	name  string
+}
 
 func main() {
+	reader := bufio.NewReader(os.Stdin)
+
 	fmt.Printf("Bienvenue sur le mini-crm !\n")
 
-	type contact struct {
-		email string
-		name  string
-	}
 	c := make(map[int]contact)
 
 	add := flag.Bool("add", false, "Ajouter un contact")
@@ -24,6 +30,9 @@ func main() {
 	flag.Parse()
 
 	if *add {
+		if *email == "" || *name == "" {
+
+		}
 		c[len(c)+1] = contact{
 			email: *email,
 			name:  *name,
@@ -34,7 +43,7 @@ func main() {
 		r := true
 
 		for r {
-			var i int
+
 			fmt.Println("Veuillez choisir une option :")
 			fmt.Println("1 : Ajouter un contact")
 			fmt.Println("2 : Lister tout les contacts")
@@ -42,71 +51,20 @@ func main() {
 			fmt.Println("4 : Modifier un contact")
 			fmt.Println("5 : Quitter le crm")
 			fmt.Println("--------------------------------")
-			fmt.Println("")
 
-			_, err := fmt.Scanln(&i)
+			input, err := reader.ReadString('\n')
+			i, _ := strconv.Atoi(strings.TrimSpace(input))
 
 			if err == nil {
 				switch i {
 				case 1:
-					fmt.Println("Veuillez spécifier une adresse mail:")
-					var a string
-					_, err1 := fmt.Scanln(&a)
-					fmt.Println("Veuillez spécifier un nom:")
-					var b string
-					_, err2 := fmt.Scanln(&b)
-					if err1 == nil && err2 == nil {
-						c[len(c)+1] = contact{
-							email: a,
-							name:  b,
-						}
-					}
-					fmt.Printf("info du contact ajouté index : %v | adresse mail %v | nom : %v \n", len(c), a, b)
+					addContact(c, reader)
 				case 2:
-					if len(c) == 0 {
-						fmt.Print("Aucun contact à afficher")
-					} else {
-						for index, val := range c {
-							fmt.Printf("index : %v | adresse mail %v | nom : %v \n", index, val.email, val.name)
-						}
-					}
+					displayContact(c)
 				case 3:
-					fmt.Println("Veuillez spécifier un index à supprimer:")
-					var index int
-					_, err1 := fmt.Scanln(&index)
-					if err1 == nil {
-						_, ok := c[index]
-						if ok {
-							delete(c, index)
-							fmt.Println("Contact supprimé")
-						} else {
-							fmt.Println("L'index séléctionné ne correspond a aucun contact")
-						}
-					}
+					deleteContact(c, reader)
 				case 4:
-					fmt.Println("Veuillez spécifier un index à modifier:")
-					var index int
-					_, err1 := fmt.Scanln(&index)
-					if err1 == nil {
-						val, ok := c[index]
-						if ok {
-							fmt.Printf("info du contact à modifier : %v | adresse mail %v | nom : %v \n", len(c), val.email, val.name)
-							fmt.Println("Veuillez spécifier une nouvelle adresse mail:")
-							var a string
-							_, err2 := fmt.Scanln(&a)
-							fmt.Println("Veuillez spécifier un nouveau nom:")
-							var b string
-							_, err3 := fmt.Scanln(&b)
-							if err2 == nil && err3 == nil {
-								c[index] = contact{
-									email: a,
-									name:  b,
-								}
-							}
-						} else {
-							fmt.Println("L'index séléctionné ne correspond a aucun contact")
-						}
-					}
+					modifyContact(c, reader)
 				case 5:
 					r = false
 				}
@@ -114,4 +72,67 @@ func main() {
 		}
 	}
 
+}
+
+func addContact(c map[int]contact, reader *bufio.Reader) {
+	fmt.Println("Veuillez spécifier une adresse mail:")
+	a, err1 := reader.ReadString('\n')
+	fmt.Println("Veuillez spécifier un nom:")
+	b, err2 := reader.ReadString('\n')
+	if err1 == nil && err2 == nil {
+		c[len(c)+1] = contact{
+			email: strings.TrimSpace(a),
+			name:  strings.TrimSpace(b),
+		}
+	}
+	fmt.Printf("info du contact ajouté index : %v | adresse mail %v | nom : %v \n", len(c), a, b)
+}
+
+func displayContact(c map[int]contact) {
+	if len(c) == 0 {
+		fmt.Println("Aucun contact à afficher")
+	} else {
+		for index, val := range c {
+			fmt.Printf("index : %v | adresse mail %v | nom : %v \n", index, val.email, val.name)
+		}
+	}
+}
+
+func deleteContact(c map[int]contact, reader *bufio.Reader) {
+	fmt.Println("Veuillez spécifier un index à supprimer:")
+	input, err1 := reader.ReadString('\n')
+	index, _ := strconv.Atoi(strings.TrimSpace(input))
+	if err1 == nil {
+		_, ok := c[index]
+		if ok {
+			delete(c, index)
+			fmt.Println("Contact supprimé")
+		} else {
+			fmt.Println("L'index séléctionné ne correspond a aucun contact")
+		}
+	}
+}
+
+func modifyContact(c map[int]contact, reader *bufio.Reader) {
+	fmt.Println("Veuillez spécifier un index à modifier:")
+	input, err1 := reader.ReadString('\n')
+	index, _ := strconv.Atoi(strings.TrimSpace(input))
+	if err1 == nil {
+		val, ok := c[index]
+		if ok {
+			fmt.Printf("info du contact à modifier : %v | adresse mail %v | nom : %v \n", len(c), val.email, val.name)
+			fmt.Println("Veuillez spécifier une nouvelle adresse mail:")
+			a, err2 := reader.ReadString('\n')
+			fmt.Println("Veuillez spécifier un nouveau nom:")
+			b, err3 := reader.ReadString('\n')
+			if err2 == nil && err3 == nil {
+				c[index] = contact{
+					email: strings.TrimSpace(a),
+					name:  strings.TrimSpace(b),
+				}
+			}
+		} else {
+			fmt.Println("L'index séléctionné ne correspond a aucun contact")
+		}
+	}
 }
